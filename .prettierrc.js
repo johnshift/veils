c0ecce0f8dd1596c1047d080b4d85ce5c38e2813
John Ballesteros <johnshift.dev@gmail.com>
2022-07-30 20:33:21 +0800

    chore: setup eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  tabWidth: 2,
+  useTabs: false,
+  importOrder: [
+    '^(next|react|react-query|@storybook/(.*))$',
+    '<THIRD_PARTY_MODULES>',
+    '^@(mantine|react-icons)/(.*)$',
+    '(@testing-library/(.*)|whatwg-fetch)$',
+    '^@(auth|shared)/(.*)$',
+    '^[./]',
+    '^[../]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  // Fix issue pretty-import not working on pnpm
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+};
