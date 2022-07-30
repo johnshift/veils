562afaf880961e6d0e9db57a8ef770a5357a36e6
John Ballesteros <johnshift.dev@gmail.com>
2022-07-30 20:41:45 +0800

    chore: setup mantine

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,14 +1,26 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { MantineProvider } from '@mantine/core';
+
 const CustomApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <title>Veils App</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
     </Head>
-    <main>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'light',
+      }}
+    >
       <Component {...pageProps} />
-    </main>
+    </MantineProvider>
   </>
 );
 
