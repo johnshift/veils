48224c86669e936546eb5efc7e7c6c8fc1d010b8
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 09:46:05 +0800

    test(ui-appbar): add menu story

---
+++ libs/shared/ui-appbar/src/lib/menu.stories.tsx
@@ -0,0 +1,55 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ReactNode, useState } from 'react';
+
+import { Menu } from './menu';
+
+export default {
+  title: 'ui-appbar/Menu',
+  component: Menu,
+} as ComponentMeta<typeof Menu>;
+
+type Props = {
+  isLoading: boolean;
+  isLoggedIn: boolean;
+  children: ReactNode;
+};
+
+const MenuWrapper = ({ isLoading, isLoggedIn, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const authFn = () => {
+    console.log(`authFn: Log${isLoggedIn ? 'out' : 'in'}`);
+  };
+
+  return (
+    <Menu
+      isOpen={isOpen}
+      isLoading={isLoading}
+      isLoggedIn={isLoggedIn}
+      setIsOpen={setIsOpen}
+      authFn={authFn}
+    />
+  );
+};
+
+const Template: ComponentStory<typeof MenuWrapper> = (args) => (
+  <MenuWrapper {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  isLoading: false,
+  isLoggedIn: false,
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  isLoading: true,
+  isLoggedIn: false,
+};
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  isLoading: false,
+  isLoggedIn: true,
+};
