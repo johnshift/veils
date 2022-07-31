7989d2dfe2e3fa33563c70721104905086bece82
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 12:09:35 +0800

    refactor(core-session): add empty session object

---
+++ libs/auth/core-session/src/lib/empty-session.ts
@@ -0,0 +1,11 @@
+import type { Session } from './types';
+
+export const emptySession: Session = {
+  id: '',
+  username: '',
+  veil: '',
+  firstName: '',
+  lastName: '',
+  avatar: '',
+  veilAvatar: '',
+};
