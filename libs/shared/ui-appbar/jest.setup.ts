17465769c03febe676e4ba972f740bca013e957f
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 09:46:35 +0800

    refactor(ui-appbar): implement resize observer for tests

--- libs/shared/ui-appbar/jest.setup.ts
+++ libs/shared/ui-appbar/jest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+// Fix: "ReferenceError: ResizeObserver is not defined"
+window.ResizeObserver =
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
