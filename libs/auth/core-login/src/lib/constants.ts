49fd42c033fd6e9aadffc80d071b956b03a7f8ef
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 14:30:21 +0800

    refactor(core-login): add login constants

---
+++ libs/auth/core-login/src/lib/constants.ts
@@ -0,0 +1,8 @@
+// Error constants
+export const ERR_LOGIN_FAILED = 'Login failed';
+
+// Messages
+export const MSG_LOGIN_OK = 'Login successful';
+
+// Urls
+export const URL_API_AUTH_LOGIN = '/api/auth/login';
