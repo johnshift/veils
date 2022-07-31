433f3bf892fe2067d3c7136669003dcff28ce96f
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 18:05:12 +0800

    refactor(core-register): add register constants

---
+++ libs/auth/core-register/src/lib/constants.ts
@@ -0,0 +1,8 @@
+// Error constants
+export const ERR_REGISTER_FAILED = 'Register failed';
+
+// Messages
+export const MSG_REGISTER_OK = 'Register successful';
+
+// Urls
+export const URL_API_AUTH_REGISTER = '/api/auth/register';
