2b099cb6a998a018a9d1a2544c7999158ddaf3e3
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 16:19:19 +0800

    refactor(core-logout): add logout constants

---
+++ libs/auth/core-logout/src/lib/constants.ts
@@ -0,0 +1,13 @@
+// Error constants
+export const ERR_LOGOUT_FAILED = 'Logout failed';
+
+// Messages
+export const MSG_LOGOUT_OK = 'Logout successful';
+export const MSG_LOGOUT_LOADING = 'You are being logged out';
+export const MSG_LOGOUT_DONE = 'You have been logged out';
+
+// Urls
+export const URL_API_AUTH_LOGOUT = '/api/auth/login';
+
+// Misc
+export const ID_LOGOUT_NOTIF = 'logout-notif-id';
