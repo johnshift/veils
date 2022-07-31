0d062560d0cadc783a1c242eeab5842e94d17444
John Ballesteros <johnshift.dev@gmail.com>
2022-07-31 14:36:13 +0800

    chore: setup auth-data-login lib

---
+++ libs/auth/data-login/jest.config.ts
@@ -0,0 +1,22 @@
+export default {
+  displayName: 'auth-data-login',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/auth/data-login',
+  collectCoverageFrom: ['<rootDir>/src/lib/**/*.{js,jsx,ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
