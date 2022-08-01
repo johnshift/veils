5d6a7737b829aa68c2416d8778f01c4d7797fdd2
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 10:52:41 +0800

    refactor(core-login): add password constants

--- libs/auth/core-login/src/lib/constants.ts
+++ libs/auth/core-login/src/lib/constants.ts
@@ -11,6 +11,10 @@ export const URL_API_AUTH_LOGIN = '/api/auth/login';
 // Lengths
 export const MIN_LENGTH_PRINCIPAL = 4;
 export const MAX_LENGTH_PRINCIPAL = 32;
+export const MIN_LENGTH_PASSWORD = 6;
+export const MAX_LENGTH_PASSWORD = 64;
 
 // Misc strings
 export const PLACEHOLDER_PRINCIPAL = 'Username/Email';
+export const PLACEHOLDER_PASSWORD = 'Password';
+export const TESTID_PASSWORD_VISIBILITY = 'toggle-password-visibility';
