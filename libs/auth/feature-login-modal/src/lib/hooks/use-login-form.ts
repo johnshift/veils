fc1ae1f03b5bec078ebabfe3a86fbefc3f5c1dda
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 16:33:38 +0800

    feat: login modal

---
+++ libs/auth/feature-login-modal/src/lib/hooks/use-login-form.ts
@@ -0,0 +1,104 @@
+import { ChangeEventHandler, DispatchWithoutAction, useEffect, useState } from 'react';
+
+import { useForm } from '@mantine/form';
+
+import { ERR_INCORRECT_LOGIN, ERR_LOGIN_FAILED } from '@auth/core-login/constants';
+import { LoginPayload } from '@auth/core-login/dto';
+import { useLoginMutation } from '@auth/data-login/fetch/use-login-mutation';
+import { validatePassword } from '@auth/ui-login-form/validation/validate-password';
+import { validatePrincipal } from '@auth/ui-login-form/validation/validate-principal';
+import { useNotify } from '@shared/util-common/notify/use-notify';
+
+// We need type `InputProps` since we only need value and onChange fields
+// returned by `@mantine/form` `getInputProps`.
+// Error field will be controlled by `isRed` internal state.
+type InputProps = {
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+};
+
+// UseLoginForm hook separates logic from ui
+export const useLoginForm = (onClose: DispatchWithoutAction, fakeLoadingMs: number) => {
+  // For login, we only give ambiguous error message
+  // isRed is the indicator for an error in validation or mutation
+  const [isRed, setIsRed] = useState(false);
+
+  // We want login to remain as ambiguous as possible (for security)
+  // But validation happens instantly, so we impolement
+  // fakeLoading to give a sense of an actual request
+  const [fakeLoading, setFakeLoading] = useState(false);
+
+  // Login form controls
+  const {
+    errors: formErrors,
+    values: formValues,
+    getInputProps,
+    onSubmit,
+    reset,
+  } = useForm<LoginPayload>({
+    initialValues: { principal: '', password: '' },
+    validate: {
+      principal: validatePrincipal,
+      password: validatePassword,
+    },
+  });
+
+  const closeModal = () => {
+    // Clear form
+    reset();
+
+    // Close modal
+    onClose();
+  };
+
+  const { mutate, error: mutationError, isLoading } = useLoginMutation(closeModal);
+
+  // We want to turn off red borders if no validation error is present
+  useEffect(() => {
+    // Should only turn off if isRed is active and no validation/mutation errors
+    const hasErrors =
+      formErrors['principal'] || formErrors['password'] || Boolean(mutationError);
+    if (isRed && !hasErrors) {
+      setIsRed(false);
+    }
+  }, [formValues, formErrors, isRed, mutationError, reset]);
+
+  // If validation error occurs, show loading notification
+  // then show an error notification after delay
+  const { notifyError } = useNotify();
+  const handleError = (errors: typeof formErrors) => {
+    if (errors['principal'] || errors['password']) {
+      setFakeLoading(true);
+      setTimeout(() => {
+        setIsRed(true);
+        notifyError(ERR_LOGIN_FAILED, ERR_INCORRECT_LOGIN);
+        setFakeLoading(false);
+      }, fakeLoadingMs);
+    }
+  };
+
+  // Submit callback fn - we call mutate fn
+  const handleSubmit = (data: typeof formValues) => mutate(data);
+
+  // InputProps `value`, `onChange` props spread on input compoennts
+  const principalInputProps = getInputProps('principal') as InputProps;
+  const passwordInputProps = getInputProps('password') as InputProps;
+
+  return {
+    // Expose close modal with form clearing
+    closeModal,
+
+    // Expose input props
+    principalInputProps,
+    passwordInputProps,
+
+    // We only want to show borders on api error or isRed
+    isInvalid: mutationError?.message === ERR_INCORRECT_LOGIN || isRed,
+
+    // Loading is either validation error fake-loading or mutation loading
+    isLoading: fakeLoading || isLoading,
+
+    // Make use of @mantine/form onSubmit handler
+    onSubmit: onSubmit(handleSubmit, handleError),
+  };
+};
---
+++ libs/auth/feature-login-modal/src/lib/login-modal.tsx
@@ -0,0 +1,90 @@
+import NextLink from 'next/link';
+
+import {
+  Anchor,
+  Button,
+  Center,
+  Group,
+  LoadingOverlay,
+  Modal,
+  Paper,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
+
+import { useSessionContext } from '@auth/data-session/context/session-context';
+import { PasswordInput } from '@auth/ui-login-form/password-input';
+import { PrincipalInput } from '@auth/ui-login-form/principal-input';
+
+import { useLoginForm } from './hooks/use-login-form';
+
+type Props = {
+  // FakeLoadingMs determines the delay for validation error notification
+  // This provides option to cut delay during tests. Defaults to 1000.
+  fakeLoadingMs?: number;
+};
+
+export const LoginModal = ({ fakeLoadingMs = 1000 }: Props) => {
+  const { openedLoginModal, closeLoginModal } = useSessionContext();
+
+  const {
+    closeModal,
+    principalInputProps,
+    passwordInputProps,
+    isInvalid,
+    isLoading,
+    onSubmit,
+  } = useLoginForm(closeLoginModal, fakeLoadingMs);
+
+  return (
+    <Modal
+      centered
+      opened={openedLoginModal}
+      withCloseButton={false}
+      size="32ch"
+      padding={0}
+      onClose={closeModal}
+    >
+      <Paper withBorder radius="md" p="xl">
+        <LoadingOverlay
+          visible={isLoading}
+          overlayBlur={0.5}
+          data-testid="login loading overlay"
+        />
+        <Stack justify="space-between" spacing="lg">
+          <form onSubmit={onSubmit}>
+            <Stack>
+              <Center>
+                <Title order={1}>veils</Title>
+              </Center>
+              <PrincipalInput
+                isInvalid={isInvalid}
+                value={principalInputProps.value}
+                onChange={principalInputProps.onChange}
+              />
+              <PasswordInput
+                isInvalid={isInvalid}
+                value={passwordInputProps.value}
+                onChange={passwordInputProps.onChange}
+              />
+
+              <Button type="submit">Login</Button>
+            </Stack>
+          </form>
+
+          <Group align="center" position="center">
+            <Text color="dimmed" size="xs">
+              Don&#39;t have an account?{' '}
+            </Text>
+            <NextLink passHref href="/register">
+              <Anchor component="a" underline={false} size="xs">
+                Register
+              </Anchor>
+            </NextLink>
+          </Group>
+        </Stack>
+      </Paper>
+    </Modal>
+  );
+};
