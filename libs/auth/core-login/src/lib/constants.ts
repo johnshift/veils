e44b54848c935a5eb75284a1ff079acab3821859
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 08:34:36 +0800

    refactor(core-login): add principal constants

--- libs/auth/core-login/src/lib/constants.ts
+++ libs/auth/core-login/src/lib/constants.ts
@@ -7,3 +7,10 @@ export const MSG_LOGIN_OK = 'Login successful';
 
 // Urls
 export const URL_API_AUTH_LOGIN = '/api/auth/login';
+
+// Lengths
+export const MIN_LENGTH_PRINCIPAL = 4;
+export const MAX_LENGTH_PRINCIPAL = 32;
+
+// Misc strings
+export const PLACEHOLDER_PRINCIPAL = 'Username/Email';
