d0c7d77daa2b51f02629214e31ab00ae4bca3201
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 22:24:23 +0800

    refactor(feature-appbar): setup storybook config

---
+++ libs/shared/feature-appbar/.storybook/main.js
@@ -0,0 +1,27 @@
+const rootMain = require('../../../../.storybook/main');
+
+module.exports = {
+  ...rootMain,
+
+  core: { ...rootMain.core, builder: 'webpack5' },
+
+  stories: [
+    ...rootMain.stories,
+    '../src/lib/**/*.stories.mdx',
+    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
+  async webpackFinal(config, { configType }) {
+    // Apply any global webpack configs that might have been specified in .storybook/main.js
+    if (rootMain.webpackFinal) {
+      config = await rootMain.webpackFinal(config, { configType });
+    }
+
+    // Add your own webpack tweaks if needed
+
+    return config;
+  },
+
+  // Import msw public mockServiceWorker
+  staticDirs: ['../../../../public'],
+};
