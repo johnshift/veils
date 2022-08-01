be14e5e6829b56e12c50736ff95a0153cc38d171
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 20:19:45 +0800

    chore: setup auth-feature-appbar lib

---
+++ libs/shared/feature-appbar/README.md
@@ -0,0 +1,4 @@
+# shared-feature-appbar
+
+This library contains appbar feature.
+Feature libraries can import from any type of library.
---
+++ libs/shared/feature-appbar/jest.config.ts
@@ -0,0 +1,25 @@
+export default {
+  displayName: 'shared-feature-appbar',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/shared/feature-appbar',
+  collectCoverageFrom: [
+    '<rootDir>/src/lib/**/*.{js,jsx,ts,tsx}',
+    '!**/*.stories.{js,jsx,ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
