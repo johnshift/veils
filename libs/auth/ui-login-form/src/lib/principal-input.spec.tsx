a0abe06fe88a973de7c0ce651baf9bda18b38113
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 11:34:21 +0800

    test(ui-login-form): add principal input unit test

---
+++ libs/auth/ui-login-form/src/lib/principal-input.spec.tsx
@@ -0,0 +1,78 @@
+import { PLACEHOLDER_PRINCIPAL } from '@auth/core-login/constants';
+import { render, screen, user } from '@shared/ui-testutils';
+
+import { PrincipalInput } from './principal-input';
+
+describe('PrincipalInput', () => {
+  test('default', async () => {
+    // Arrange props
+    const isLoading = false;
+    const isInvalid = false;
+    const value = '';
+    const onChange = jest.fn();
+
+    render(
+      <PrincipalInput
+        isLoading={isLoading}
+        isInvalid={isInvalid}
+        value={value}
+        onChange={onChange}
+      />,
+    );
+
+    // Assert default
+    const input = screen.getByPlaceholderText(PLACEHOLDER_PRINCIPAL);
+    expect(input).toBeEnabled();
+    expect(input).toBeRequired();
+    expect(input).toHaveAttribute('type', 'text');
+    expect(input).toHaveAttribute('aria-invalid', 'false');
+    expect(input).toHaveValue(value);
+
+    // Assert onChange
+    await user.type(input, '12345');
+    expect(onChange).toHaveBeenCalledTimes(5);
+  });
+
+  test('isLoading', async () => {
+    // Arrange props
+    const isLoading = true;
+    const isInvalid = false;
+    const value = '';
+    const onChange = jest.fn();
+
+    render(
+      <PrincipalInput
+        isLoading={isLoading}
+        isInvalid={isInvalid}
+        value={value}
+        onChange={onChange}
+      />,
+    );
+
+    // Assert disabled
+    expect(screen.getByPlaceholderText(PLACEHOLDER_PRINCIPAL)).toBeDisabled();
+  });
+
+  test('isInvalid', async () => {
+    // Arrange props
+    const isLoading = false;
+    const isInvalid = true;
+    const value = '';
+    const onChange = jest.fn();
+
+    render(
+      <PrincipalInput
+        isLoading={isLoading}
+        isInvalid={isInvalid}
+        value={value}
+        onChange={onChange}
+      />,
+    );
+
+    // Assert disabled
+    expect(screen.getByPlaceholderText(PLACEHOLDER_PRINCIPAL)).toHaveAttribute(
+      'aria-invalid',
+      'true',
+    );
+  });
+});
