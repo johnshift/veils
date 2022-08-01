a1b5b43fb03322adff896d4c8a7605bcd460bf88
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 09:09:03 +0800

    refactor(util-common): add notify loading fn

---
+++ libs/shared/util-common/src/lib/notify/use-notify-loading.tsx
@@ -0,0 +1,60 @@
+import { v4 } from 'uuid';
+
+import {
+  cleanNotifications,
+  showNotification,
+  updateNotification,
+} from '@mantine/notifications';
+import { IconCircleCheck } from '@tabler/icons';
+import { IconAlertCircle } from '@tabler/icons';
+
+import { MSG_LOADING } from '@shared/core-common/constants';
+
+export const useNotifyLoading = () => {
+  const id = v4();
+
+  // Loading notification
+  const notifyLoading = (message: string) => {
+    cleanNotifications();
+    showNotification({
+      id,
+      color: 'blue',
+      title: MSG_LOADING,
+      message,
+      loading: true,
+      autoClose: false,
+      disallowClose: true,
+    });
+  };
+
+  // Success notification
+  const notifySuccess = (title: string, message: string) => {
+    updateNotification({
+      id,
+      title,
+      message,
+      color: 'green',
+      icon: <IconCircleCheck />,
+      autoClose: 2000,
+    });
+  };
+
+  // Error notification
+  const notifyError = (title: string, message: string) => {
+    updateNotification({
+      id,
+      title,
+      message,
+      color: 'red',
+      icon: <IconAlertCircle />,
+      autoClose: 9000,
+    });
+  };
+
+  return {
+    id,
+    notifyLoading,
+    notifySuccess,
+    notifyError,
+  };
+};
