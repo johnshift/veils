3a458ebfd856c0a2e754f5c44847d8bc0430cae9
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 13:27:50 +0800

    refactor(data-testutils): add user event reexport

--- libs/shared/data-testutils/src/index.ts
+++ libs/shared/data-testutils/src/index.ts
@@ -1,3 +1,6 @@
+import userEvent from '@testing-library/user-event';
+
+// Wrapper
 export { TestWrapper } from './lib/test-wrapper';
 
 // Re-export
@@ -5,3 +8,6 @@ export * from '@testing-library/react';
 
 // Override render
 export { customRender as render } from './lib/custom-render';
+
+// User event
+export const user = userEvent.setup();
