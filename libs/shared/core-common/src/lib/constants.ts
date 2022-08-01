f1fa1fdd4d9168d585583bfad540cf815aa317c2
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 08:34:06 +0800

    refactor(core-common): add regex constants

--- libs/shared/core-common/src/lib/constants.ts
+++ libs/shared/core-common/src/lib/constants.ts
@@ -1,3 +1,8 @@
 // Error messages
 export const ERR_INTERNAL = 'Something went wrong :(';
 export const ERR_NETWORK = 'Network request failed';
+
+// Regex
+export const REGEX_PROFILE_ID = /^[A-Za-z][\dA-Za-z-]*$/;
+export const REGEX_EMAIL = /^[\w&*+-]+(?:\.[\w&*+-]+)*@(?:[\dA-Za-z-]+\.)+[A-Za-z]{2,7}$/;
+export const REGEX_NAME = /^[A-Za-z]+$/;
