d64900ccfd3152dc00cefe16a04e914a2c77d25b
John Ballesteros <johnshift.dev@gmail.com>
2022-08-01 11:34:29 +0800

    test(ui-login-form): add password input unit test

---
+++ libs/auth/ui-login-form/src/lib/password-input.spec.tsx
@@ -0,0 +1,85 @@
+import {
+  PLACEHOLDER_PASSWORD,
+  TESTID_PASSWORD_VISIBILITY,
+} from '@auth/core-login/constants';
+import { render, screen, user } from '@shared/ui-testutils';
+
+import { PasswordInput } from './password-input';
+
+describe('PasswordInput', () => {
+  test('default', async () => {
+    // Arrange props
+    const isLoading = false;
+    const isInvalid = false;
+    const value = '';
+    const onChange = jest.fn();
+
+    render(
+      <PasswordInput
+        isLoading={isLoading}
+        isInvalid={isInvalid}
+        value={value}
+        onChange={onChange}
+      />,
+    );
+
+    // Assert default
+    const input = screen.getByPlaceholderText(PLACEHOLDER_PASSWORD);
+    expect(input).toBeEnabled();
+    expect(input).toBeRequired();
+    expect(input).toHaveAttribute('type', 'password');
+    expect(input).not.toHaveAttribute('aria-invalid');
+    expect(input).toHaveValue(value);
+
+    // Assert onChange
+    await user.type(input, '12345');
+    expect(onChange).toHaveBeenCalledTimes(5);
+
+    // Assert toggle password
+    const toggleVisibility = screen.getByTestId(TESTID_PASSWORD_VISIBILITY);
+    await user.click(toggleVisibility);
+    expect(input).toHaveAttribute('type', 'text');
+    await user.click(toggleVisibility);
+    expect(input).toHaveAttribute('type', 'password');
+  });
+
+  test('isLoading', async () => {
+    // Arrange props
+    const isLoading = true;
+    const isInvalid = false;
+    const value = '';
+    const onChange = jest.fn();
+
+    render(
+      <PasswordInput
+        isLoading={isLoading}
+        isInvalid={isInvalid}
+        value={value}
+        onChange={onChange}
+      />,
+    );
+
+    // Assert disabled
+    expect(screen.getByPlaceholderText(PLACEHOLDER_PASSWORD)).toBeDisabled();
+  });
+
+  test('isInvalid', async () => {
+    // Arrange props
+    const isLoading = false;
+    const isInvalid = true;
+    const value = '';
+    const onChange = jest.fn();
+
+    render(
+      <PasswordInput
+        isLoading={isLoading}
+        isInvalid={isInvalid}
+        value={value}
+        onChange={onChange}
+      />,
+    );
+
+    // Assert disabled
+    expect(screen.getByPlaceholderText(PLACEHOLDER_PASSWORD).parentNode).toBeInvalid();
+  });
+});
