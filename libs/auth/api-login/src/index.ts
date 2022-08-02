34339bf5e809db50acc8e65fa88f903d31ca1c41
John Ballesteros <johnshift.dev@gmail.com>
2022-08-02 13:08:03 +0800

    refactor(api-login): moved login, get-email rpc into api lib

---
+++ libs/auth/api-login/src/index.ts
@@ -0,0 +1,3 @@
+// Rpc
+export { login } from './lib/rpc/login';
+export { getEmail } from './lib/rpc/get-email';
--- libs/auth/data-login/src/index.ts
+++ libs/auth/data-login/src/index.ts
@@ -1,6 +1,2 @@
 // Queries/Mutations
 export { useLoginMutation } from './lib/fetch/use-login-mutation';
-
-// Rpc
-export { login } from './lib/rpc/login';
-export { getEmail } from './lib/rpc/get-email';
