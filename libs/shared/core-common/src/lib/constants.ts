87e293955af10fa9025f1f7176129401f136b5d6
John Ballesteros <johnshift.dev@gmail.com>
2022-08-02 13:28:21 +0800

    refactor(core-common): add http error constants

--- libs/shared/core-common/src/lib/constants.ts
+++ libs/shared/core-common/src/lib/constants.ts
@@ -1,6 +1,9 @@
 // Error messages
 export const ERR_INTERNAL = 'Something went wrong :(';
 export const ERR_NETWORK = 'Network request failed';
+export const ERR_INVALID_REQUEST = 'Invalid request';
+export const ERR_METHOD_NOT_ALLOWED = 'Method not allowed';
+export const ERR_UNAUTHORIZED = 'Unauthorized';
 
 // Regex
 export const REGEX_PROFILE_ID = /^[A-Za-z][\dA-Za-z-]*$/;
