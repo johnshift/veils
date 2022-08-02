924a4bdec34f1272e3a5c37167702d56e8115e43
John Ballesteros <johnshift.dev@gmail.com>
2022-08-02 12:03:19 +0800

    chore: setup auth-api-session lib

---
+++ libs/auth/api-session/README.md
@@ -0,0 +1,4 @@
+# auth-api-session
+
+This library contains api-handlers and rpc for session. It should only be used in the backend.
+Lint is set to import `type:core` and `type:util` libraries only.
---
+++ libs/auth/api-session/jest.config.ts
@@ -0,0 +1,22 @@
+export default {
+  displayName: 'auth-api-session',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/auth/api-session',
+  collectCoverageFrom: ['<rootDir>/src/lib/**/*.{js,jsx,ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
